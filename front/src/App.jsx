ebb0c8c683b9d49cfae70bd7722dc2ce05586376
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2024-10-19 18:11:05 +0300

    ...

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,6 +8,7 @@ import LoginQrCode from './components/authenfication/LoginQrCode';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import './assets/main.js';
 
 
 function App() {
--- front/src/components/dashboard/Header.jsx
+++ front/src/components/dashboard/Header.jsx
@@ -51,8 +51,8 @@ const Header = () => {
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
                 <li class="dropdown-header">
-                    You have 4 new notifications
-                    <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                    4 nouveaux notifications
+                    {/* <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">Voir tout</span></a> */}
                 </li>
                 <li>
                     <hr class="dropdown-divider"/>
@@ -110,7 +110,7 @@ const Header = () => {
                     <hr class="dropdown-divider"/>
                 </li>
                 <li class="dropdown-footer">
-                    <a href="#">Show all notifications</a>
+                    <a href="#">Voir tout les notifications</a>
                 </li>
 
                 </ul>
@@ -129,8 +129,8 @@ const Header = () => {
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
                 <li class="dropdown-header">
-                    You have 3 new messages
-                    <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                    3 nouveau messages
+                    {/* <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">Voir tout</span></a> */}
                 </li>
                 <li>
                     <hr class="dropdown-divider"/>
@@ -140,9 +140,9 @@ const Header = () => {
                     <a href="#">
                     <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle"/>
                     <div>
-                        <h4>Maria Hudson</h4>
-                        <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                        <p>4 hrs. ago</p>
+                        <h4>Manoa</h4>
+                        <p>Mba atero ny bokiko azafady.</p>
+                        <p>Il y a 4min</p>
                     </div>
                     </a>
                 </li>
@@ -154,9 +154,9 @@ const Header = () => {
                     <a href="#">
                     <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle"/>
                     <div>
-                        <h4>Anna Nelson</h4>
-                        <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                        <p>6 hrs. ago</p>
+                        <h4>Allan</h4>
+                        <p>Ela efa nagnano evaluation va ? </p>
+                        <p>Il y a 2h</p>
                     </div>
                     </a>
                 </li>
@@ -168,9 +168,9 @@ const Header = () => {
                     <a href="#">
                     <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle"/>
                     <div>
-                        <h4>David Muldon</h4>
-                        <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                        <p>8 hrs. ago</p>
+                        <h4>Mansour</h4>
+                        <p>Karakory  ma laolo</p>
+                        <p>Il y a 8h</p>
                     </div>
                     </a>
                 </li>
@@ -179,7 +179,7 @@ const Header = () => {
                 </li>
 
                 <li class="dropdown-footer">
-                    <a href="#">Show all messages</a>
+                    <a href="#">Voir tout les messages</a>
                 </li>
 
                 </ul>
@@ -192,14 +192,14 @@ const Header = () => {
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                 <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle"/>
-                <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                <span class="d-none d-md-block dropdown-toggle ps-2">1110 H-F</span>
                 </a>
                 {/* <!-- End Profile Iamge Icon --> */}
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                 <li class="dropdown-header">
-                    <h6>Kevin Anderson</h6>
-                    <span>Web Designer</span>
+                    <h6>Nambinina Hasina</h6>
+                    <span>M1 IG</span>
                 </li>
                 <li>
                     <hr class="dropdown-divider"/>
@@ -208,7 +208,7 @@ const Header = () => {
                 <li>
                     <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                     <i class="bi bi-person"></i>
-                    <span>My Profile</span>
+                    <span>Mon Profil</span>
                     </a>
                 </li>
                 <li>
@@ -218,7 +218,7 @@ const Header = () => {
                 <li>
                     <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                     <i class="bi bi-gear"></i>
-                    <span>Account Settings</span>
+                    <span>Paramètre</span>
                     </a>
                 </li>
                 <li>
@@ -228,7 +228,7 @@ const Header = () => {
                 <li>
                     <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
                     <i class="bi bi-question-circle"></i>
-                    <span>Need Help?</span>
+                    <span>Besoin d'aide ?</span>
                     </a>
                 </li>
                 <li>
@@ -238,7 +238,7 @@ const Header = () => {
                 <li>
                     <a class="dropdown-item d-flex align-items-center" href="#">
                     <i class="bi bi-box-arrow-right"></i>
-                    <span>Sign Out</span>
+                    <span>Deconnexion</span>
                     </a>
                 </li>
 
